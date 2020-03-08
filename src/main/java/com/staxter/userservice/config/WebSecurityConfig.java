25a227d390e45bb23ca4265cd06c2dab44e71011
jj <jjyong90@gmail.com>
2020-03-08 13:31:44 +0100

    Add web security configuration with disabled security

---
+++ src/main/java/com/staxter/userservice/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.staxter.userservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/**
+ * The type Web security config.
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    http.httpBasic().disable();
+    http.csrf().disable();
+  }
+
+}
